781e5728de690441da209fd5061d3d65f9313205
Deepak <deepamdeebu@gmail.com>
2023-01-11 21:00:51 +0530

    Initialized laravel project and added traveller model, seeder and migration with sample data

---
+++ app/Models/Traveler.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Traveler extends Model
+{
+    use HasFactory;
+
+    /**
+     *  Disable created at and updated at timestamp
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id',
+        'traveller_name',
+    ];
+}
